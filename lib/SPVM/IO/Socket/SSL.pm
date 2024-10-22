b2ecf7c43a6972a1426245f43d3266f6eb7a4d45
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-10-22 09:29:51 +0900

    Initial commit.

---
+++ lib/SPVM/IO/Socket/SSL.pm
@@ -0,0 +1,65 @@
+package SPVM::IO::Socket::SSL;
+
+our $VERSION = "0.001";
+
+1;
+
+=head1 Name
+
+SPVM::IO::Socket::SSL - Short Description
+
+=head1 Description
+
+IO::Socket::SSL class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use IO::Socket::SSL;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
